8ab828d2d58a987e0d1dba37433056fe49ad2185
Saidur1170 <112820365+Saidur1214@users.noreply.github.com>
2024-12-16 23:59:25 +0600

    v2

--- script.js
+++ script.js
@@ -95,13 +95,190 @@
 
 // console.log(profile)
 
-let ab = (a, b) => console.log(a+b)
+// let ab = (a, b) => console.log(a+b)
 
-console.log(ab(5, 10))
+// console.log(ab(5, 10))
 
-let abc = (a,b, forc) => forc(ab/c)
+// let abc = (a,b, forc) => forc(ab/c)
 
-console.log(abc(2))
+// console.log(abc(2))
 
 // let namegreet = greet(usename) => (usename)
 // console.log(namegreet(saidur), "bro")
+
+
+
+// Pure Testing
+
+
+
+
+
+// let age = 12
+
+// if (age >= 18){
+//     console.log("adult")
+// }
+
+// else if (age >=12){
+//     console.log("teenager")
+//     alert("Your IP will be blocked for our safty")
+// }
+
+// else{
+//     console.log("kid")
+//     alert("Your IP will be blocked for your safty")
+// }
+
+// function bribe(age) {
+//     if (age>=12 && age<=18) {
+//         newage = age == 12 ? (age + 6) : (age + 8)
+//         newage = age == 13 ? (age + 5) : (age + 8)
+//         return newage
+//     } 
+    
+//     else if (age <12 ) {
+//         console.log("Go away Kid!")
+        
+//     }
+    
+//     else {
+//         console.log("Listen, Bro I am a good person I dont do these!")
+        
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+// Loop in Javascript
+
+// let a = ["saidur", "kawser", "rahman"]
+
+// console.log(a.length)
+
+// for (let i = 0; i < a.length; i++) {
+//     const element = a[i]; 
+//     console.log(element)
+// }
+
+// for (const char of "saidur") {
+//     console.log(char)
+// }
+
+
+// let random = Math.random()
+// console.log(random)
+// let a = prompt("Enter first number")
+// let c = prompt("Enter operation")
+// let b = prompt("Enter second number")
+
+// let obj = {
+//     "+": "-",
+//     "*": "+",
+//     "-": "/",
+//     "/": "**",
+// }
+
+
+
+// if (random > 0.1) {
+//     Perform correct calculation
+//     console.log(`The result is ${a} ${c} ${b}`)
+//     alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
+// }
+
+// else {
+//     Perform wrong calculation
+//     c = obj[c]
+//     alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+
+// }
+
+let arr = [1,5,10,15,20]
+
+// function greaterthanten(arr) {
+//     if (arr > 10) {
+//         return true
+//     } 
+//     return false
+// }
+
+// console.log(arr.filter(greaterthanten))
+
+
+// const morethanten = (arr)=> {
+//     if(arr>10){
+//         return true
+//     }
+//     return false
+// }
+
+// console.log(arr.filter(greaterthanten))
+
+
+// console.log(arr.filter((arr)=>{ 
+//     if(arr>10){
+//                 return true
+//             }
+//             return false
+        
+// }))
+
+
+// let rand = Math.random()
+// let first, second, third;
+// // 0 0.33 0.66 1
+
+
+// // Lets generate the first word
+// if(rand<0.33){
+//     first = "Crazy"
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     first = "Amazing"
+// }
+// else{
+//     first = "Fire"
+// }
+
+// // Lets generate the second word
+// rand = Math.random()
+// if(rand<0.33){
+//     second = "Engine"
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     second = "Foods"
+// }
+// else{
+//     second = "Garments"
+// }
+
+// // Lets generate the third word
+// rand = Math.random()
+// if(rand<0.33){
+//     third = "Bros"
+// }
+// else if(rand<0.66 && rand>=0.33){
+//     third = "Limited"
+// }
+// else{
+//     third = "Hub"
+// }
+
+// console.log(`${first} ${second} ${third}`)
+
+
+// let div = document.createElement('div')
+// div.innerHTML = "<h1> Hello how are you?</h1>"
+
+
+// document.body.append(div)
+
